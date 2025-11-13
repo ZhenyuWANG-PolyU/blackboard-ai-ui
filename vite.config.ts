c13691f1d8062d264bd1bafb29ea651af399460c
ZhenyuWANG <zhenyu25.wang@connect.polyu.hk>
2025-11-14 05:59:52 +0800

    更新 API 代理目标端口为 44333

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://209.38.57.222',
+        target: 'http://209.38.57.222:44333',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
